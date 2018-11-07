d2ee3d766c61136b2eba248975fc4d9279569c00
Thomas Jolley <tomjolley81@gmail.com>
2018-11-07 17:44:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
