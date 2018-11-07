60e110b69164dfa67c90416d489407bd993d7cc1
Thomas Jolley <tomjolley81@gmail.com>
2018-11-07 17:47:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,10 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array) {
+  return array.slice(-1)
+}
